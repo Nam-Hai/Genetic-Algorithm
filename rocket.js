31045b02d8eef153f9442c5c65c48ccae3dcc1ea
BlueTompon <lucastran00@gmail.com>
2022-02-24 19:35:39 +0100

    1.0

--- rocket.js
+++ rocket.js
@@ -1,18 +1,51 @@
 class Rocket {
-    constructor() {
-        this.pos = createVector(width / 2, height);
-        this.vel = p5.Vector.random2D();
+    constructor(dna) {
+        this.pos = createVector(width / 2, height - 1);
+        this.vel = createVector();
         this.acc = createVector();
+
+        if (dna) {
+            this.dna = dna;
+        } else {
+            this.dna = new DNA();
+        }
+
+        this.crashed = false;
+        this.win = false;
+        this.age = 0;
+        this.point = 0;
     }
 
     applyForce(force) {
         this.acc.add(force);
     }
 
+    calcPoint() {
+        let d = dist(this.pos.x, this.pos.y, target.x, target.y);
+
+        this.point = 1 / (1 + d);
+
+        if (this.win) {
+            let speedPoint = map(this.age, 0, 200, 1, 0, true)
+            this.point += speedPoint;
+            this.point *= 3;
+        }
+        if (this.crashed || (!this.crashed && this.win)) {
+            let agePoint = map(this.age, 0, 200, 0, 1, true) / 2;
+            this.point += agePoint;
+        }
+    }
+
     update() {
+        this.applyForce(this.dna.genes[counter])
+        this.stopTest();
+        if (this.crashed || this.win) return
+
+        this.age = counter;
         this.vel.add(this.acc);
         this.pos.add(this.vel);
         this.acc.mult(0);
+
     }
 
     show() {
@@ -21,8 +54,30 @@ class Rocket {
         rotate(this.vel.heading())
         rectMode(CENTER);
         noStroke()
-        fill(50)
-        rect(0, 0, 50, 10);
+        fill(50, 50, 50, 120)
+        rect(0, 0, 30, 5);
         pop();
+    }
+
+    stopTest() {
+        let d = dist(this.pos.x, this.pos.y, target.x, target.y);
+        if (d < 10) {
+            this.win = true;
+        }
+
+        // Stop at edge
+        if (this.pos.x < 0 || this.pos.x > width || this.pos.y < 0 || this.pos.y > height) {
+            this.crashed = true;
+        }
+
+        // stop at obstacle
+
+        if (this.pos.x > rectX && this.pos.x < rectX + rectW && this.pos.y > rectY && this.pos.y < rectY + rectH) {
+            this.crashed = true;
+        }
+
+        if (this.pos.x > rectX + rectW + 25 && this.pos.x < rectX + rectW + 25 + rectW && this.pos.y > rectY && this.pos.y < rectY + rectH) {
+            this.crashed = true;
+        }
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,20 +1,58 @@
 let rocket;
 let population;
+let counter;
+let lifeP;
+let target;
+const speed = 0.3;
+
+const rectX = 100;
+const rectY = 200;
+const rectW = 150;
+const rectH = 10;
+
+const generationCountDiv = document.querySelector(".generationCount");
+let generation = 0;
+const lifespan = 200;
+
 
 function setup() {
   createCanvas(400, 300);
   population = new Population();
+  counter = 0;
+
+
+  lifeP = createP();
+
+  target = createVector(width / 2, 50)
 }
 
 function draw() {
   background(220);
   population.run()
+  lifeP.html(counter);
+  counter++;
+
+  if (counter == lifespan) {
+    population.evaluate();
+    population.selection()
+    counter = 0;
+    generation++;
+    generationCountDiv.textContent = "Generation : " + generation;
+  }
+
+  // Obstacle
+  rect(rectX, rectY, rectW, rectH);
+
+  rect(rectX + rectW + 25, rectY, rectW, rectH)
+  // Target
+  ellipse(target.x, target.y, 16, 16)
 }
 
 class Population {
   constructor() {
     this.rockets = [];
-    this.popsize = 25;
+    this.popsize = 70;
+    this.matingPool = [];
 
     for (let i = 0; i < this.popsize; i++) {
       this.rockets.push(new Rocket())
@@ -28,4 +66,97 @@ class Population {
     }
   }
 
+  evaluate() {
+    let maxPoint = 0;
+
+    for (let roc of this.rockets) {
+      roc.calcPoint();
+      if (roc.point > maxPoint) {
+        maxPoint = roc.point;
+      }
+    }
+
+    // Normalize points
+    for (let roc of this.rockets) {
+      roc.point /= maxPoint;
+    }
+
+    this.matingPool = [];
+
+    // increase chances for better rockets to be picked (to mate)
+    for (let roc of this.rockets) {
+      let n = roc.point * 40;
+
+      for (let j = 0; j < n; j++) {
+        this.matingPool.push(roc);
+      }
+    }
+
+  }
+
+  selection() {
+    let newRockets = [];
+
+    for (let i = 0; i < this.rockets.length; i++) {
+      let parentA = random(this.matingPool).dna;
+      let parentB = random(this.matingPool).dna;
+
+      let child = parentA.crossoverOnePoint(parentB);
+      child.mutation();
+      newRockets.push(new Rocket(child))
+    }
+    this.rockets = newRockets;
+  }
+}
+
+class DNA {
+  constructor(genes) {
+    if (genes) {
+      this.genes = genes;
+    } else {
+      this.genes = [];
+
+      for (let i = 0; i < lifespan; i++) {
+        this.genes.push(p5.Vector.random2D())
+        this.genes[i].setMag(speed)
+      }
+    }
+  }
+
+  crossoverOnePoint(partnerDNA) {
+    let newGenes = [];
+    let mid = floor(random(this.genes.length));
+
+    for (let i = 0; i < this.genes.length; i++) {
+      if (i > mid) {
+        newGenes.push(this.genes[i]);
+      } else {
+        newGenes.push(partnerDNA.genes[i]);
+      }
+    }
+    return new DNA(newGenes)
+  }
+
+  crossoverUniform(partnerDNA) {
+    let newGenes = [];
+    for (let i = 0; i < this.genes.length; i++) {
+      let r = random(0, 2);
+      if (r <= 1) {
+        newGenes.push(this.genes[i]);
+      } else {
+        newGenes.push(partnerDNA.genes[i]);
+      }
+
+    }
+    return new DNA(newGenes)
+  }
+
+  mutation() {
+    for (let gene of this.genes) {
+      if (random(1) < 0.5) {
+        gene = p5.Vector.random2D();
+        gene.setMag(speed)
+      }
+    }
+  }
 }
