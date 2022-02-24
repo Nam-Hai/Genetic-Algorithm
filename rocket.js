a3eec718401585a3a1ba9ed6e6f97f30dadd85a6
BlueTompon <lucastran00@gmail.com>
2022-02-24 17:09:55 +0100

    first launche

--- rocket.js
+++ rocket.js
@@ -1,7 +1,7 @@
 class Rocket {
     constructor() {
-        this.pos = createVector();
-        this.vel = createVector();
+        this.pos = createVector(width / 2, height);
+        this.vel = p5.Vector.random2D();
         this.acc = createVector();
     }
 
@@ -13,5 +13,16 @@ class Rocket {
         this.vel.add(this.acc);
         this.pos.add(this.vel);
         this.acc.mult(0);
+    }
+
+    show() {
+        push();
+        translate(this.pos.x, this.pos.y);
+        rotate(this.vel.heading())
+        rectMode(CENTER);
+        noStroke()
+        fill(50)
+        rect(0, 0, 50, 10);
+        pop();
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,31 @@
+let rocket;
+let population;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 300);
+  population = new Population();
 }
 
 function draw() {
   background(220);
+  population.run()
+}
+
+class Population {
+  constructor() {
+    this.rockets = [];
+    this.popsize = 25;
+
+    for (let i = 0; i < this.popsize; i++) {
+      this.rockets.push(new Rocket())
+    }
+  }
+
+  run() {
+    for (let i = 0; i < this.popsize; i++) {
+      this.rockets[i].update();
+      this.rockets[i].show();
+    }
+  }
+
 }
